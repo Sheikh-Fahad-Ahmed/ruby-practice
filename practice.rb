fab508983bfa7b884ccd724153f529cd8f16d510
Fahad Ahmed <fahad838@gmail.com>
2024-08-22 20:24:45 +0530

    Some practice of methods

--- practice.rb
+++ practice.rb
@@ -87,23 +87,66 @@
 #
 
 
-top_five_games = ["mario brothers",
-                  "excite bike",
-                  "ring king",
-                  "castlevania",
-                  "double dragon"]
+# top_five_games = ["mario brothers",
+#                   "excite bike",
+#                   "ring king",
+#                   "castlevania",
+#                   "double dragon"]
 
-top_five_games.each_with_index do |item, index|
-  puts "#{index + 1}. #{item}"
+# top_five_games.each_with_index do |item, index|
+#   puts "#{index + 1}. #{item}"
+# end
+
+
+# arr1 = [1,2,3,4,5]
+# arr2 = []
+
+# arr1.each do |item|
+#   arr2 << item + 2
+# end
+
+# p arr1 
+# p arr2
+
+
+# a = [1,2,3]
+
+# def mutate(arr)
+#   arr.pop
+# end
+
+# p "Before mutate method: #{a}"
+# mutate(a)
+# p "After mutate method: #{a}"
+
+
+
+
+# Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string.
+
+def greeting(name)
+  puts "Hello, #{name}"
 end
 
+greeting "fahad"
 
-arr1 = [1,2,3,4,5]
-arr2 = []
 
-arr1.each do |item|
-  arr2 << item + 2
+puts x = 2
+
+p name = 'joe'
+
+
+# Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+
+def multiply(num1, num2) 
+  num1 * num2 
 end
 
-p arr1 
-p arr2
+puts multiply(2,3)
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
